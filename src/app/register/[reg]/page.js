8c65fdb1732f25b57403b9b555664018dcb95c71
gyuni1337 <silviplosakov@gmail.com>
2024-03-28 14:34:16 +0100

    register page design

--- src/app/register/[reg]/page.js
+++ src/app/register/[reg]/page.js
@@ -10,7 +10,6 @@ export default function Page({params}) {
     useEffect(() => {
         async function fetchVehicle() {
             const data = await findByRegister(params.reg);
-            console.log(vehicle);
             setVehicle(data[0]);
         }
 
@@ -18,6 +17,56 @@ export default function Page({params}) {
     }, []);
 
   return (
-    <div>{ vehicle.length !== 0 && <h1>{vehicle.brand}</h1>}</div>
+    <>
+    { vehicle !== undefined ? (
+    <div className='flex justify-center w-max mx-auto text-center hover:-translate-y-1 transition-transform mt-32 '>
+      <div className=' h-[600px] text-black p-20 bg-white rounded-2xl shadow-md shadow-white'>
+        <h1 className='text-3xl mb-5'>Fordonsdata</h1>
+        <div className='flex flex-col gap-2'>
+          <div className='flex gap-2 justify-center items-center'>
+            <p className='text-xl'>Registreringsnummer:</p>
+            <div className='bg-gray-200 p-2 rounded-lg'>{vehicle.registration}</div>
+          </div>
+          <div className='flex items-center justify-center gap-2'>
+            <p className='text-xl'>Brand:</p>
+            <div className='bg-gray-200 p-2 rounded-lg'>{vehicle.brand}</div>
+          </div>
+          <div className='flex gap-2 justify-center items-center'>
+            <p className='text-xl'>Årsmodell:</p>
+            <div className='bg-gray-200 p-2 rounded-lg'>{vehicle.year}</div>
+          </div>
+          <div className='flex gap-2 justify-center items-center'>
+            <p className='text-xl'>Färg:</p>
+            <div className='bg-gray-200 p-2 rounded-lg'>{vehicle.color}</div>
+          </div>
+          <div className='flex gap-2 justify-center items-center'>
+            <p className='text-xl'>Antal hjul:</p>
+            <div className='bg-gray-200 p-2 px-4 rounded-lg'>{vehicle.wheels}</div>
+          </div>
+          <div className='flex gap-2 justify-center items-center'>
+            <p className='text-xl'>Typ:</p>
+            <div className='bg-gray-200 p-2 rounded-lg'>{vehicle.type}</div>
+          </div>
+          <div className='flex flex-col gap-2 justify-center mt-10'>
+                <div>
+                <button className='border-2 border-black p-1 px-10 rounded-lg text-xl hover:bg-black hover:text-white transition-colors duration-200'>Redigera</button>
+                </div>
+                <div>
+                <button className='border-2 border-black p-1 px-3 rounded-lg text-xl hover:bg-black hover:text-white transition-colors duration-200'>Radera fordonet</button>
+
+                </div>
+                
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    ) : 
+      <>
+
+      </>
+  }
+    </>
   )
 }
